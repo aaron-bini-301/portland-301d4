3c35d6fbad1573f9bce7e0c44531c77a9728ff7c
a-she <al@alswebdesigns.net>
2016-06-09 10:51:22 -0700

    mob coded. Chart not connected

--- demoCode/class04_emVsRem/app.js
+++ demoCode/class04_emVsRem/app.js
@@ -1,11 +1,13 @@
-//$(function() {
-var canvas = $('canvas')[0];
+$(function() { // Doc ready
+
+var canvas = $('canvas')[0]; // Plain DOM element at index 0
 Ndivs = 5;
 
+// Label array for chart
 var divNames = [];
 for (var ii = 0; ii < Ndivs; ii++) { divNames.push('Level ' + ii); }
 
-// Dummy data
+// Dummy data - replace during mob coding
 function getDivWidths(arg) {
   if (arg) { return [10, 20, 10, 20, 0] };
   return [.5, 1, 2, 4, 8];
@@ -20,13 +22,14 @@ var ch = new Chart(canvas, {
   data: {
     labels: divNames,
     datasets: [{ label: 'Set A div widths',
-                 data: dwA  // div widths
+                 data: dwA  // Set A div widths
                },
                { type: 'line',
                  label: 'Set B div widths',
-                 data: dwB  // div widths
+                 data: dwB  // Set B div widths
                }],
     options: { scales: { yAxes: [{ ticks: { beginAtZero:true } }] } }
   }
 });
-//});
+
+});
